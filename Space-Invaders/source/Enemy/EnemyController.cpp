80b449f2a4fa8e10abb555329365d5f8d7b72d7b
rugved20 <143347845+rugved20@users.noreply.github.com>
2024-05-10 03:02:41 +0530

    Update EnemyController.cpp

--- Space-Invaders/source/Enemy/EnemyController.cpp
+++ Space-Invaders/source/Enemy/EnemyController.cpp
@@ -7,110 +7,90 @@
 #include "../../header/Player/PlayerController.h"
 #include "../../header/Particle/ParticleSystem.h"
 #include "../../header/Sound/SoundService.h"
+#include <random>
 
 namespace Enemy
 {
-	using namespace Global;
-	using namespace Time;
-	using namespace Collision;
-	using namespace Entity;
-	using namespace Player;
-	using namespace Sound;
+    using namespace Global;
+    using namespace Time;
+    using namespace Collision;
+    using namespace Entity;
+    using namespace Player;
+    using namespace Sound;
 
-	EnemyController::EnemyController(EnemyType type)
-	{
-		enemy_view = new EnemyView();
-		enemy_model = new EnemyModel(type);
-	}
+    EnemyController::EnemyController(EnemyType type)
+    {
+        enemy_view = std::make_unique<EnemyView>();
+        enemy_model = std::make_unique<EnemyModel>(type);
+    }
 
-	EnemyController::~EnemyController()
-	{
-		delete (enemy_view);
-		delete (enemy_model);
-	}
+    void EnemyController::initialize()
+    {
+        enemy_model->initialize();
+        enemy_model->setEnemyPosition(getRandomInitialPosition());
+        enemy_view->initialize(this);
+    }
 
-	void EnemyController::initialize()
-	{
-		enemy_model->initialize();
-		enemy_model->setEnemyPosition(getRandomInitialPosition());
-		enemy_view->initialize(this);
-	}
+    void EnemyController::update()
+    {
+        move();
+        enemy_view->update();
+        handleOutOfBounds();
+    }
 
-	void EnemyController::update()
-	{
-		move();
-		enemy_view->update();
-		handleOutOfBounds();
-	}
+    void EnemyController::render()
+    {
+        enemy_view->render();
+    }
 
-	void EnemyController::render()
-	{
-		enemy_view->render();
-	}
+    sf::Vector2f EnemyController::getRandomInitialPosition()
+    {
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_real_distribution<float> disX(enemy_model->left_most_position.x, enemy_model->right_most_position.x);
+        std::uniform_real_distribution<float> disY(enemy_model->left_most_position.y, enemy_model->right_most_position.y);
+        float x_position = disX(gen);
+        float y_position = disY(gen);
+        return sf::Vector2f(x_position, y_position);
+    }
 
-	sf::Vector2f EnemyController::getRandomInitialPosition()
-	{
-		float x_offset_distance = (std::rand() % static_cast<int>(enemy_model->right_most_position.x - enemy_model->left_most_position.x));
-		float x_position = enemy_model->left_most_position.x + x_offset_distance;
-		float y_position = enemy_model->left_most_position.y;
+    void EnemyController::handleOutOfBounds()
+    {
+        sf::Vector2f enemyPosition = getEnemyPosition();
+        sf::Vector2u windowSize = ServiceLocator::getInstance()->getGraphicService()->getGameWindow()->getSize();
 
-		return sf::Vector2f(x_position, y_position);
-	}
+        if (enemyPosition.x < 0 || enemyPosition.x > windowSize.x ||
+            enemyPosition.y < 0 || enemyPosition.y > windowSize.y)
+        {
+            destroy();
+        }
+    }
 
-	void EnemyController::handleOutOfBounds()
-	{
-		sf::Vector2f enemyPosition = getEnemyPosition();
-		sf::Vector2u windowSize = ServiceLocator::getInstance()->getGraphicService()->getGameWindow()->getSize();
+    sf::Vector2f EnemyController::getEnemyPosition() const
+    {
+        return enemy_model->getEnemyPosition();
+    }
 
-		if (enemyPosition.x < 0 || enemyPosition.x > windowSize.x ||
-			enemyPosition.y < 0 || enemyPosition.y > windowSize.y) 
-		{
-			ServiceLocator::getInstance()->getEnemyService()->destroyEnemy(this);
-		}
-	}
+    void EnemyController::onCollision(ICollider* other_collider)
+    {
+        if (auto player_controller = dynamic_cast<PlayerController*>(other_collider))
+        {
+            processScore();
+            destroy();
+        }
+    }
 
-	sf::Vector2f EnemyController::getEnemyPosition()
-	{
-		return enemy_model->getEnemyPosition();
-	}
+    void EnemyController::processScore()
+    {
+        ServiceLocator::getInstance()->getPlayerService()->increaseEnemiesKilled(1);
+    }
 
-	EnemyState EnemyController::getEnemyState()
-	{
-		return enemy_model->getEnemyState();
-	}
+    void EnemyController::destroy()
+    {
+        ServiceLocator::getInstance()->getParticleService()->spawnParticleSystem(enemy_model->getEnemyPosition(),
+            Particle::ParticlesType::EXPLOSION);
 
-	EnemyType EnemyController::getEnemyType()
-	{
-		return enemy_model->getEnemyType();
-	}
-
-	const sf::Sprite& EnemyController::getColliderSprite()
-	{
-		return enemy_view->getEnemySprite();
-	}
-
-	void EnemyController::onCollision(ICollider* other_collider)
-	{
-		PlayerController* player_controller = dynamic_cast<PlayerController*>(other_collider);
-		if (player_controller)
-		{
-			processScore();
-			destroy();
-			return;
-		}
-	}
-
-	void EnemyController::processScore()
-	{
-		ServiceLocator::getInstance()->getPlayerService()->increaseEnemiesKilled(1);
-	}
-
-	void EnemyController::destroy()
-	{
-		ServiceLocator::getInstance()->getParticleService()->spawnParticleSystem(enemy_model->getEnemyPosition(),
-			Particle::ParticlesType::EXPLOSION);
-
-		ServiceLocator::getInstance()->getSoundService()->playSound(SoundType::EXPLOSION);
-		ServiceLocator::getInstance()->getEnemyService()->destroyEnemy(this);
-	}
+        ServiceLocator::getInstance()->getSoundService()->playSound(SoundType::EXPLOSION);
+        ServiceLocator::getInstance()->getEnemyService()->destroyEnemy(this);
+    }
 }
